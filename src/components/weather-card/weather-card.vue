516a667468d41bdfd7dc4efa4570f96ebeee92e4
Roman Kamenivskyi <rk@wwwest.solutions>
2023-07-18 15:37:06 +0300

    add new components

---
+++ src/components/weather-card/weather-card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="c-weather-card">
+    <div class="c-weather-card__header">
+      <input v-model="value" class="c-input" type="search" placeholder="Search" />
+      <AppButton variant="primary" pill>Add to chosen</AppButton>
+    </div>
+    <div class="c-weather-card__tabs">
+      <AppButton>Day</AppButton>
+      <AppButton>Week</AppButton>
+    </div>
+    <WeatherCardData />
+    <WeatherCardChart />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppButton from '@/components/app-button.vue';
+import WeatherCardData from '@/components/weather-card/weather-card-data.vue';
+import WeatherCardChart from '@/components/weather-card/weather-card-chart.vue';
+
+const value = ref('');
+</script>
--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>Home view</main>
+  <main>
+    <div class="container">
+      <WeatherCard />
+    </div>
+  </main>
 </template>
+
+<script setup lang="ts">
+import WeatherCard from '@/components/weather-card/weather-card.vue';
+</script>
