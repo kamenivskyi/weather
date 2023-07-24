4450ee93b00b3b32b6d85703c2fef19a1cb094c0
Roman Kamenivskyi <rk@wwwest.solutions>
2023-07-24 12:51:22 +0300

    change router to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/home-view.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [ 
     {
       path: '/',
       name: 'home',
